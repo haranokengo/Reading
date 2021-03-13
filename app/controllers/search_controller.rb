b06de428181c9540c02b32b8e9611fede8270887
haranokengo <haranokengo@gmail.com>
2021-03-13 08:59:09 +0000

    検索機能実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,21 @@
 class SearchController < ApplicationController
   
   def search
-    @value = params["search"]["value"]         #データを代入
-    @how = params["search"]["how"]             #データを代入
-    @datas = search_for(@how, @value)      
+    @value = params["search"]["value"]
+    @how = params["search"]["how"]
+    # search_forアクションでReviewモデルから対象のcategory_idと紐付いたreviewを@datasに格納
+    @datas = search_for(@how, @value)
   end
   
   private
-
+  
   def match(value)
-    #.orを使用することで、valueに一致するカラムのデータをnameカラムとgenre_idカラムから探してきます。
-    Review.where(content: value).or(Review.where(category_id: value))
+    Review.where(category_id: value)
   end
 
   def search_for(how, value)
-    case how                     #引数のhowと一致する処理に進むように定義しています。
-    when 'match'                 #ジャンル検索の場合はmatchで固定してるので、必ず'match'の処理に進みます。
+    case how
+    when 'match'
       match(value)
     end
   end
